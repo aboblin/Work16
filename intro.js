886fe6971219ba5d9a1026e843f9f644d2a9f06b
GordonLei <glei@stuy.edu>
2017-12-10 17:36:38 -0500

    pushed

--- intro.js
+++ intro.js
@@ -1,15 +1,38 @@
 var h = document.getElementById('h');
 var originalHeader = h.innerHTML;
+var list = document.getElementById("thelist");
+var listElements = document.getElementsByTagName("li");
+
+
 
 var replaceCallback = function(e) {
     h.innerHTML = e.path[0].innerHTML;
 };
-var thelist = document.getElementById('thelist');
 
 var originalCallback = function(e) {
     console.log(e);
     h.innerHTML = originalHeader;
 };
 
-thelist.addEventListener( 'mouseover' , replaceCallback );
-thelist.addEventListener( 'mouseout' , originalCallback );
+var addElement = function(e){
+    var num = listElements.length; //gives us the # of the next pos since len is 1 more than pos
+    var temp = document.createElement("li");
+    temp.innerHTML = "item " + num;
+    list.appendChild(temp);
+};
+
+//button orders matter. You can't use the code below if u put it with the other variables at the top
+var button = document.getElementById("b");
+button.addEventListener("click", addElement)
+//----
+
+var removeElement = function(e){
+    this.remove();
+}
+
+list.addEventListener( 'mouseover' , replaceCallback );
+list.addEventListener( 'mouseout' , originalCallback );
+
+for (num = 0; num < listElements.length; num++){
+    listElements[num].addEventListener("click", removeElement)
+}
