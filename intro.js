8c84530d3ede202fc43bf102cd7902f1e735bc7b
aboblin <aboylan@stuy.edu>
2017-12-11 13:40:10 -0500

    edited intro.js

--- intro.js
+++ intro.js
@@ -1,7 +1,10 @@
 var h = document.getElementById('h');
 var originalHeader = h.innerHTML;
 var list = document.getElementById("thelist");
+var otherlist = document.getElementById("theotherlist");
 var listElements = document.getElementsByTagName("li");
+var fibNum = 0;
+//var otherlistElements = document.getElementsByTagName("li");
 
 var replaceCallback = function(e) {
     h.innerHTML = e.path[0].innerHTML;
@@ -19,10 +22,21 @@ var addElement = function(e){
     list.appendChild(temp);
 };
 
+var addFibonacci = function(e){
+    //var num = listElements.length; //gives us the # of the next pos since len is 1 more than pos
+    var temp = document.createElement("li");
+    temp.innerHTML = "item" + fibonacci(fibNum);
+    fibNum += 1;
+    otherlist.appendChild(temp);
+};
+
 //button orders matter. You can't use the code below if u put it with the other variables at the top
 var button = document.getElementById("b");
-button.addEventListener("click", addElement)
+button.addEventListener("click", addElement);
 //----
+
+var otherbutton = document.getElementById("ob");
+otherbutton.addEventListener("click", addFibonacci);
 
 var removeElement = function(e){
     this.remove();
@@ -32,7 +46,6 @@ var fibonacci = function(n) {
     if (n < 1) {return 1;};
     return fibonacci(n - 2) + fibonacci(n - 1);
 };
-
 
 
 list.addEventListener( 'mouseover' , replaceCallback );
